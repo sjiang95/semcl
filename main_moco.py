c9dc7719b6cf69f8c3eb4c1db5c31f653eb878c3
Shengjiang Quan <qsj287068067@126.com>
2022-02-17 19:33:06 +0900

    typo
    Signed-off-by: Shengjiang Quan <qsj287068067@126.com>

--- main_moco.py
+++ main_moco.py
@@ -147,8 +147,7 @@ def main():
                       'from checkpoints.')
 
     if args.gpu is not None:
-        warnings.warn('You have chosen a specific GPU. This will completely '
-                      'disable data parallelism.')
+        warnings.warn('You have chosen a specific GPU. This will completely disable data parallelism.')
 
     if args.dist_url == "env://" and args.world_size == -1:
         args.world_size = int(os.environ["WORLD_SIZE"])
